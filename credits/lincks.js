09fe3162b43eadb81318a17a5c6eca75d020365c
vuyghu2020 <111427114+vuyghu2020@users.noreply.github.com>
2023-06-18 17:54:37 +0200

    Update lincks.js

--- credits/lincks.js
+++ credits/lincks.js
@@ -43,15 +43,10 @@ cradit_text = document.createElement("p");
 
 cradit_text.innerText = "Created by vuyghu2020"
 cradit_text.style.width = "154px"
-cradit_text.style.position = "relative"
+cradit_text.style.position = "fixed"
 
-cradit_text.style.top = innerHeight - 36
-cradit_text.style.left = innerWidth - 172
-
-setInterval(() => {
-    cradit_text.style.top = innerHeight - 36
-    cradit_text.style.left = innerWidth - 172
-}, 0);
+cradit_text.style.bottom = 0;
+cradit_text.style.right = 10;
 
 
 
